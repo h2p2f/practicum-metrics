7ec4d96167abded07edf9172311d7cad1acbf7b4
h2p2f <h2p2f@sonx.me>
2023-04-30 13:53:16 +0300

    iter9  - added file storage, initial data load from file

--- internal/server/handlers/middlewares.go
+++ internal/server/handlers/middlewares.go
@@ -6,14 +6,26 @@ import (
 )
 
 // GzipHanle is middleware for gzip
-func GzipHanle(h http.Handler) http.Handler {
+func GzipHanle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		originWriter := w
 		acceptEncoding := r.Header.Get("Accept-Encoding")
 		contentEncoding := r.Header.Get("Content-Encoding")
 		supportGzip := strings.Contains(acceptEncoding, "gzip")
 		sendGzip := strings.Contains(contentEncoding, "gzip")
-		if supportGzip && sendGzip {
+
+		if !supportGzip {
+			next.ServeHTTP(w, r)
+			return
+		}
+		if supportGzip && !sendGzip {
+			originWriter := w
+			compressedWriter := NewCompressWriter(w)
+			originWriter = compressedWriter
+			defer compressedWriter.Close()
+			next.ServeHTTP(originWriter, r)
+		}
+		if sendGzip {
+			originWriter := w
 			compressedWriter := NewCompressWriter(w)
 			originWriter = compressedWriter
 			defer compressedWriter.Close()
@@ -23,9 +35,10 @@ func GzipHanle(h http.Handler) http.Handler {
 			}
 			r.Body = compressedReader
 			defer compressedReader.Close()
-		}
 
-		h.ServeHTTP(originWriter, r)
+			next.ServeHTTP(originWriter, r)
+
+		}
 	})
 
 }
